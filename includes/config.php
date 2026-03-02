6ba8860d771990559569785ef14debae00692b59
sze lum <_szelum_@szes-MacBook-Air.local>
2026-03-02 12:41:46 +0800

    Add config.php with database connection

---
+++ includes/config.php
@@ -0,0 +1,21 @@
+<?php
+/* CONFIG.PHP - Database Connection
+Purpose: Connects PHP to MySQL database
+*/
+
+// MySQL connection details
+$host = "localhost";
+$user ="root";
+$password = "root";
+$database = "COMP1044_Database";
+
+// Create connection
+$conn = new mysqli($host, $user, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Database connected successfully!";
+?>
